6d9e41dc499c38ccf49c7d434c43f7c8f40f268f
Oussama Belabbas <oussamaoussama2016@gmail.com>
2022-09-11 12:21:55 +0100

    tested tflite

--- lib/home.dart
+++ lib/home.dart
@@ -29,31 +29,31 @@ class _HomePageState extends State<HomePage> {
 
   loadModel() async {
     String res;
-    switch (_model) {
-      case yolo:
-        res = (await Tflite.loadModel(
-          model: "assets/yolov2_tiny.tflite",
-          labels: "assets/yolov2_tiny.txt",
-        ))!;
-        break;
+    try {
+      switch (_model) {
+        case yolo:
+          res = (await Tflite.loadModel(
+            model: "assets/yolov2_tiny.tflite",
+            labels: "assets/yolov2_tiny.txt",
+          ))!;
+          break;
 
-      case mobilenet:
-        res = (await Tflite.loadModel(
-            model: "assets/mobilenet_v1_1.0_224.tflite",
-            labels: "assets/mobilenet_v1_1.0_224.txt"))!;
-        break;
+        case mobilenet:
+          res = (await Tflite.loadModel(
+              model: "assets/mobilenet_v1_1.0_224.tflite", labels: "assets/mobilenet_v1_1.0_224.txt"))!;
+          break;
 
-      case posenet:
-        res = (await Tflite.loadModel(
-            model: "assets/posenet_mv1_075_float_from_checkpoints.tflite"))!;
-        break;
+        case posenet:
+          res = (await Tflite.loadModel(model: "assets/posenet_mv1_075_float_from_checkpoints.tflite"))!;
+          break;
 
-      default:
-        res = (await Tflite.loadModel(
-            model: "assets/ssd_mobilenet.tflite",
-            labels: "assets/ssd_mobilenet.txt"))!;
+        default:
+          res = (await Tflite.loadModel(model: "assets/ssd_mobilenet.tflite", labels: "assets/ssd_mobilenet.txt"))!;
+      }
+      print(res);
+    } catch (e) {
+      print(e);
     }
-    print(res);
   }
 
   onSelect(model) {
@@ -99,22 +99,18 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             )
-          : Stack(
-              children: [
-                Camera(
-                  widget.cameras,
-                  _model,
-                  setRecognitions,
-                ),
-                BndBox(
-                    _recognitions == null ? [] : _recognitions,
-                    math.max(_imageHeight, _imageWidth),
-                    math.min(_imageHeight, _imageWidth),
-                    screen.height,
-                    screen.width,
-                    _model),
-              ],
-            ),
+          : Center(child: Text('Test')),
+      // Stack(
+      //     children: [
+      //       Camera(
+      //         widget.cameras,
+      //         _model,
+      //         setRecognitions,
+      //       ),
+      //       BndBox(_recognitions == null ? [] : _recognitions, math.max(_imageHeight, _imageWidth),
+      //           math.min(_imageHeight, _imageWidth), screen.height, screen.width, _model),
+      //     ],
+      //   ),
     );
   }
 }
